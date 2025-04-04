a9779c4f336cdd4f226db42a2a14c96fbc2f1b75
Elpeep27 <elpeepyoulloa@gmail.com>
2025-04-04 13:18:01 -0600

    cambio de escala de imagenes y vistas

--- logicaprincipal.js
+++ logicaprincipal.js
@@ -5,4 +5,19 @@ window.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('scrolled');
     }
+
+    const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100; // Corregido: usa window.scrollY
+    let bgColor;
+    if (scrollPercentage < 20) {
+        bgColor = '#043434';
+    } else if (scrollPercentage < 40) {
+        bgColor = '#0c7169';
+    } else if (scrollPercentage < 60) {
+        bgColor = '#21b1a6';
+    } else if (scrollPercentage < 80) {
+        bgColor = '#34e0d6';
+    } else {
+        bgColor = '#e6f4f1';
+    }
+    document.body.style.backgroundImage = `linear-gradient(to bottom, ${bgColor}, #e6f4f1)`;
 });
